b66eb49ef356a69fb5cc7c2209663fc568ff85ce
NicoPicotto <nicopicotto@hotmail.com>
2023-01-19 00:40:42 -0300

    firebase condicional

--- src/Components/CardProducto/CardProducto.js
+++ src/Components/CardProducto/CardProducto.js
@@ -10,46 +10,56 @@ import {
 } from '@chakra-ui/react';
 
 const CardProducto = ({ nombre, codigo, imagen, multiplo }) => {
-	const [cantidad, setCantidad] = useState(multiplo);
+	const [cantidad, setCantidad] = useState(0);
 
 	const sumar = (cantidad) => {
 		let sumado = cantidad + multiplo;
 		setCantidad(sumado);
 	};
 
-    const restar = (cantidad) => {
-        if (cantidad >= multiplo) {
-            let restado = cantidad - multiplo;
-            setCantidad(restado)
-        }
-        
-    }
+	const restar = (cantidad) => {
+		if (cantidad >= multiplo) {
+			let restado = cantidad - multiplo;
+			setCantidad(restado);
+		}
+	};
 
 	return (
-		<Flex w='250px' h='250px' flexDir='column' alignItems='center'>
-			<Text>{nombre}</Text>
-			<Text>{codigo}</Text>
-			<Image src={imagen} />
-			<Flex alignItems='center' p={1}>
-				<Text>Cantidad: </Text>
+		<Flex
+			w='250px'
+			flexDir='column'
+			alignItems='center'
+			bgColor='white'
+			p={2}
+            borderRadius={5}
+            shadow="md"
+		>
+			<Text as="b">{nombre}</Text>
+			<Text fontSize="sm" as="i">{codigo}</Text>
+			<Image src={imagen} w="150px" p={2}/>
+			<Flex alignItems='center' w="80%" marginBottom={1}>
+				<Button
+					bgColor='color.secundario'
+					color='white'
+					_hover={{ bgColor: 'color.primario' }}
+					onClick={() => restar(cantidad)}
+                    margin={1}
+				>
+					-
+				</Button>
+				<Input type='number' value={cantidad} textAlign='center' margin={1}/>
+
 				<Button
 					bgColor='color.secundario'
 					color='white'
 					_hover={{ bgColor: 'color.primario' }}
 					onClick={() => sumar(cantidad)}
+                    margin={1}
 				>
 					+
 				</Button>
-				<Input type='number' value={cantidad} textAlign='center' />
-				<Button
-					bgColor='color.secundario'
-					color='white'
-					_hover={{ bgColor: 'color.primario' }}
-                    onClick={() => restar(cantidad)}
-				>
-					-
-				</Button>
 			</Flex>
+            <Button w="100%" colorScheme="green">Agregar</Button>
 		</Flex>
 	);
 };
--- src/Components/Lista/Lista.js
+++ src/Components/Lista/Lista.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../../firebase';
-import { collection, query, onSnapshot } from 'firebase/firestore';
+import { collection, query, onSnapshot, where } from 'firebase/firestore';
 import {
 	Flex,
 	Heading,
@@ -18,7 +18,7 @@ const Lista = () => {
 
 	useEffect(() => {
 		setLoading(true);
-		const q = query(collection(db, 'productos'));
+		const q = query(collection(db, 'productos'), where('estado', '==', true));
 		const unsub = onSnapshot(q, (querySnapshot) => {
 			let productosArray = [];
 			querySnapshot.forEach((doc) => {
@@ -35,14 +35,14 @@ const Lista = () => {
 		<Flex w='65%' flexDir='column' p={5}>
 			<Heading>Seleccioná tus productos</Heading>
 			<Divider borderColor='color.primario' marginBlock={2} />
-			<Flex flexDir='column'>
+			<Flex wrap='wrap' gap={5} p={2}>
 				{productos.map((producto) => (
 					<CardProducto
 						key={producto.codigo}
 						nombre={producto.nombre}
 						codigo={producto.codigo}
 						imagen={producto.imagen}
-                        multiplo={producto.multiplo}
+						multiplo={producto.multiplo}
 					/>
 				))}
 			</Flex>
